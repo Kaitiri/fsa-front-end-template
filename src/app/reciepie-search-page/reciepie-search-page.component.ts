425fb670f3d2c8fbc35e8819216cc4a9f406242b
Kaitiri <nenekaitirri@gmail.com>
2021-11-15 18:09:54 +0100

    innitial commit

---
+++ src/app/reciepie-search-page/reciepie-search-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reciepie-search-page',
+  templateUrl: './reciepie-search-page.component.html',
+  styleUrls: ['./reciepie-search-page.component.css']
+})
+export class ReciepieSearchPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
